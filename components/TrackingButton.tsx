eb0afcf7d00dd7efa8180a664bcf6a2906118fdc
IAMKOTARO <saitou.kotarou898@mail.kyutech.jp>
2021-10-09 23:16:56 +0900

    :+1:add firebase, _upload function

--- components/TrackingButton.tsx
+++ components/TrackingButton.tsx
@@ -2,6 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
 import { Subscription } from '@unimodules/react-native-adapter';
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAv1ThhbZo9GkjBNoKGfhHZalo0Uby-5Ew",
+  authDomain: "bicycle-supporter.firebaseapp.com",
+  projectId: "bicycle-supporter",
+  storageBucket: "bicycle-supporter.appspot.com",
+  messagingSenderId: "879836047331",
+  appId: "1:879836047331:web:c9427267150ead733e22d6",
+  measurementId: "G-W3VT1BKM6S"
+};
+
+try {
+  firebase.initializeApp(firebaseConfig);
+} catch (error) {
+  console.log(error);
+  throw error;
+}
+const db = firebase.firestore();
 
 export default function App() {
   const [acc, setData] = useState({
@@ -12,7 +32,9 @@ export default function App() {
   const [subscription, setSubscription] = useState<Subscription | null>(null);
   const [buffer, setBuffer] = useState({
     acc: new Array(),
-  })
+  });
+  const [startAt, setStartAt] = useState(new Date());
+  const [endAt, setEndAt] = useState(new Date);
 
   const _subscribe = () => {
     setSubscription(
@@ -20,12 +42,15 @@ export default function App() {
         setData(accelerometerData);
       })
     );
+    setStartAt(new Date());
     Accelerometer.setUpdateInterval(100);
   };
 
   const _unsubscribe = () => {
     subscription && subscription.remove();
     setSubscription(null);
+    setEndAt(new Date());
+    _upload();
     _reset();
   };
 
@@ -38,7 +63,19 @@ export default function App() {
     setBuffer({
       acc: new Array(),
     });
-  }
+  };
+
+  const _upload = () => {
+    const experimentRef = db.collection('experiment');
+    let document = experimentRef.doc();
+    document.set({
+      lastUpdatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+      startAt: startAt,
+      duration: Math.floor(endAt.getTime() - startAt.getTime()) / 1000,
+      csvPath: `experiment/${document.id}`,
+    });
+    console.log(document.id);
+  };
 
   useEffect(() => {
     setBuffer({
